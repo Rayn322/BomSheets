9a8f9af9a00253b2eb225dda0a3eef953b3edfb7
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-07-29 21:26:03 -0700

    clean up a little

--- main.ts
+++ main.ts
@@ -3,15 +3,11 @@ import * as XLSX from 'https://cdn.sheetjs.com/xlsx-0.20.3/package/xlsx.mjs';
 
 const workbook = XLSX.readFile('Order 1 BOMs.xlsx');
 const firstSheetName = workbook.SheetNames[0];
-// console.log(workbook.Sheets[firstSheetName]);
 
 const json = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName], {
-	range: 'B2:H1000',
+	range: 'B2:H999', // 999 is just a big number ig
 });
 
 for (const row of json) {
 	console.log(row);
-	// if (row.Quantity == 'Quantity') {
-	// 	console.log(row);
-	// }
 }
