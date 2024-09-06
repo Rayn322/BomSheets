9e5a775b1b7923a03b2b447bf7143a8e6ab26d97
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-09-05 20:13:03 -0700

    testing whatever

--- testing.ts
+++ testing.ts
@@ -1,6 +1,7 @@
+// @deno-types="npm:@types/js-quantities"
 import Qty from 'npm:js-quantities/esm';
 
-const cap1 = Qty('0.1uF');
-const cap2 = Qty('100nF');
+const cap1 = Qty('0.1');
+const cap2 = Qty(1 / 10);
 
 console.log(cap1.eq(cap2)); // true, since 0.1uF = 100nF
