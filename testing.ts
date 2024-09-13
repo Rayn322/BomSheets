a1710d20e52d5f1f63c67d4f1e1d84181449249f
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-09-12 20:25:16 -0700

    Weird type checking

--- testing.ts
+++ testing.ts
@@ -1,7 +1,12 @@
-// @deno-types="npm:@types/js-quantities"
-import Qty from 'npm:js-quantities/esm';
+const bro = 'bro' as string | undefined;
 
-const cap1 = Qty('0.1');
-const cap2 = Qty(1 / 10);
+if (!broIsString(bro)) {
+	Deno.exit();
+}
 
-console.log(cap1.eq(cap2)); // true, since 0.1uF = 100nF
+// error if bro is undefined
+console.log(bro.length);
+
+function broIsString(bro: string | undefined): bro is string {
+	return typeof bro === 'string' && 1 < 2;
+}
